ef882cd2109eae0bfd30a9cebc8b22bbcd233e69
Ross Perry <perryr16@gmail.com>
2021-02-16 10:18:01 -0700

    basic formatting on 20 asteroids functional

--- asteroids/asteroids.py
+++ asteroids/asteroids.py
@@ -3,9 +3,19 @@ import os
 import json
 
 def asteroid_closest_approach():
-    res = browse_neos()
-    breakpoint()
-    return res
+    asteroids = []
+    nasa_data = browse_neos()
+    asteroids = find_closest_approach_and_append(nasa_data, asteroids)
+    
+    # for asteroid in asteroids['near_earth_objects']:
+    #   asteroids['close_approach_data'] = min(asteroid['close_approach_data'], key=lambda x: x['miss_distance']['astronomical'])
+    return asteroids
+
+def find_closest_approach_and_append(nasa_data, asteroids):
+  for asteroid in nasa_data['near_earth_objects']:
+    asteroid['close_approach_data'] = min(asteroid['close_approach_data'], key=lambda close_approach: close_approach['miss_distance']['astronomical'])
+    asteroids.append(asteroid)
+  return asteroids
 
 def browse_neos(page_num = 0):
     key = os.getenv('NASA_KEY')
--- tests/test_asteroids.py
+++ tests/test_asteroids.py
@@ -18,6 +18,18 @@ def test_version():
 #     assert raw_keys == ['links', 'page', 'near_earth_objects']
 
 def test_asteroid_closest_approach():
-    res = asteroid_closest_approach()
-    keys = list(res)
-    assert keys == ['asteroids']
+    asteroids = asteroid_closest_approach()
+    assert asteroids[0]['links']
+    assert asteroids[0]['id']
+    assert asteroids[0]['neo_reference_id']
+    assert asteroids[0]['name']
+    assert asteroids[0]['name_limited']
+    assert asteroids[0]['designation']
+    assert asteroids[0]['nasa_jpl_url']
+    assert asteroids[0]['absolute_magnitude_h']
+    assert asteroids[0]['estimated_diameter']
+    assert asteroids[0]['is_potentially_hazardous_asteroid'] == False
+    assert asteroids[0]['close_approach_data']
+    assert asteroids[0]['close_approach_data']
+    assert asteroids[0]['orbital_data']
+    assert asteroids[0]['is_sentry_object'] == False
