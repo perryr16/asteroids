37d61e9b4a1ec75c495294717c3c784ae98b184b
Ross Perry <perryr16@gmail.com>
2021-02-18 15:04:05 -0700

    delete old file

--- tests/test_vcr.py
+++
@@ -1,6 +0,0 @@
-# import vcr
-# import urllib2
-
-# with vcr.use_cassette('fixtures/vcr_cassettes/synopsis.yaml'):
-#     response = urllib2.urlopen('http://www.iana.org/domains/reserved').read()
-#     assert 'Example domains' in response
